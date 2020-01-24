bd713fb3750e68e14f94998e396cf58c8b9611a5
Jasper Michalke <jaspermichalke@Jaspers-MacBook-Pro.local>
2020-01-24 10:35:36 +0100

    Fixed html string

--- lib/katex_flutter.dart
+++ lib/katex_flutter.dart
@@ -2,6 +2,7 @@ library katex_flutter;
 
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -85,6 +86,7 @@ class _KaTeXState extends State<KaTeX> {
 
   @override
   Widget build(BuildContext context) {
+    if(kIsWeb) return(Text(widget.laTeX));
     return SizedBox(
       height: _height,
       width: _width,
@@ -106,6 +108,7 @@ class _KaTeXState extends State<KaTeX> {
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css" integrity="sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq" crossorigin="anonymous">
 <script defer src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js" integrity="sha384-y23I5Q6l+B6vatafAwxRu/0oK/79VlbSz7Q9aiSZUvyWYIYsd+qj+o24G5ZU2zJz" crossorigin="anonymous"></script>
 <script defer src="https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/contrib/auto-render.min.js" integrity="sha384-kWPLUVMOks5AQFrykwIup5lo0m3iMkkHrD0uJ4H5cjeGihAutqP0yW0J6dpFiVkI" crossorigin="anonymous"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
 <script>
   document.addEventListener("DOMContentLoaded", function() {
     renderMathInElement(document.querySelector("#katex_flutter"),{delimiters: [
